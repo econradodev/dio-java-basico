29412e6938248a4117d12ca8b9624ae621e7bd37
Conrado <econradodev@gmail.com>
2023-08-24 09:11:27 -0300

    atualiza tipos de variaveis

--- java-tipos-variaveis/tipos-variaveis/src/TiposVariaveis.java
+++ java-tipos-variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int numero = 10;
+
+        double salarioMinimo =  3100.50;
+
+        final double VALOR_DE_PI = 3.14;
+        // convenção para ser uma constante;
+
+        String meuNome = "Emanuel Conrado";
+
     }
 }
