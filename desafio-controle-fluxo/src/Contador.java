918f736497b512026a11e50413d7a337d4666f9f
Emanuel Conrado <120610095+econradodev@users.noreply.github.com>
2023-08-26 18:02:24 -0300

    Desafio DIO Contador em Java

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -24,7 +24,6 @@ public class Contador {
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
         }
-		
 		int contagem = parametroDois - parametroUm;
 		for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
